9925b701cc3cf4e0f50f5a5d33fb846a2fb74599
abhir <abhiramashok1062004@gmail.com>
2024-08-18 23:00:50 +0530

    Initial Commit

--- src/pages/AcademicDiscounts.jsx
+++ src/pages/AcademicDiscounts.jsx
@@ -14,10 +14,26 @@ function AcademicDiscounts() {
                 heading: "JetBrains",
                 link: "https://www.jetbrains.com/shop/eform/students",
                 description: "Get Jet Brains products free for students using College mail id.",
-            },{
+            },
+            {
                 heading: "Github Developer pack",
                 link: "https://education.github.com/pack",
                 description: " GitHub Student Developer Pack.",
+            },
+            {
+                heading: "MS Office 365",
+                link: "https://www.microsoft.com/en-us/education/products/office",
+                description: "Office 365 Education is free for students with a valid college mail address.",
+            },
+            {
+                heading: "AWS Educate",
+                link: "https://aws.amazon.com/education/awseducate/",
+                description: "Amazon,s global initiative to provide students comprehensive resources.",
+            },
+            {
+                heading: "Bitbucket Education",
+                link: "https://bitbucket.org/product/education",
+                description: "A Git-based source code repository hosting service owned by Atlassian.",
             },
         ]);
     },[]);
