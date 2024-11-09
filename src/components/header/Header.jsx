19cbe6e71ff456f3f5a8e80391658eecd18d2e4b
abhir <abhiramashok1062004@gmail.com>
2024-11-09 14:42:30 +0530

    Added Contribution Description

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
                 <Link to={'/contribute'} className={'pullRequest'} onClick={() => {}}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                          stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-                         className="icon icon-tabler icons-tabler-outline icon-tabler-message">
+                         className="icon-tabler icons-tabler-outline icon-tabler-message">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                         <path d="M8 9h8"/>
                         <path d="M8 13h6"/>
--- src/pages/PullRequest.jsx
+++ src/pages/PullRequest.jsx
@@ -1,4 +1,5 @@
 import {Link} from "react-router-dom";
+import '/src/components/header/header.css'
 
 export default function PullRequest() {
     return <>
